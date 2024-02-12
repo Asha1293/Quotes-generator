71f8560b47eb025930c95a5b90ee0606256e0b8c
Asha jacob <asha.j.1993@gmail.com>
2024-02-12 17:00:03 +0100

    Updated files

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-const api_url = https://api.quotable.io/random
+
+
+let quotesSpace = document.getElementById("quotes")
+let authorSpace = document.getElementById("author")
+
+
+
+const api_url = "https://api.quotable.io/random"
+
+async function getquotes(url){
+
+let response = await fetch(url)
+
+let data = await response.json()
+
+console.log(data)
+
+quotesSpace.innerHTML = data.content
+authorSpace.innerHTML = data.author
+   
+}
+
+function tweet(){
+    window.open("https://twitter.com/intent/tweet?text=" + quotesSpace.innerHTML + " ---by " + authorSpace.innerHTML, "Tweet Window", "width=700", "height=300")
+}
+
+
+
+
